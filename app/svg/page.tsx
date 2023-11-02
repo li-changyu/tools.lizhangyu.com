55844745f57e1463e37ae88a6cd59742b7cc2d11
mxy8319 <2047776003@qq.com>
2023-11-02 16:36:57 +0800

    feat: 添加一键复制功能及简单的提示

--- app/svg/page.tsx
+++ app/svg/page.tsx
@@ -1,10 +1,30 @@
 'use client';
 import Button from "@/ui/button";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import ClipboardJS from 'clipboard'
+import clsx from "clsx";
+
+const SVG_COPY_ID = 'svg-clipboard-input'
 
 export default function Page() {
   const textareaInput = useRef<HTMLTextAreaElement>(null)
   const [svgOutput, setSvgOutput] = useState('')
+  const [toastVisible, setToastVisible] = useState(false)
+
+  useEffect(() => {
+    const clipboard = new ClipboardJS(`#${SVG_COPY_ID}`)
+    clipboard.on('success', () => setToastVisible(true))
+    return () => {
+      clipboard.destroy()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!toastVisible) return
+    setTimeout(() => {
+      setToastVisible(false)
+    }, 1000)
+  }, [toastVisible])
 
   return (
     <>
@@ -25,12 +45,14 @@ export default function Page() {
       }}>压缩</Button>
 
       <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
-        <div className="rounded-lg bg-black p-3.5 lg:p-6">
+        <div className="relative rounded-lg bg-black p-3.5 lg:p-6">
+          <div className="absolute right-[8px] top-[8px] w-[24px] h-[24px] bg-[url('/copy.svg')] bg-center bg-no-repeat opacity-70 cursor-pointer hover:opacity-90" id={SVG_COPY_ID} data-clipboard-text={svgOutput}></div>
           <div className="prose prose-sm prose-invert max-w-none min-h-[300px]">
             {svgOutput}
           </div>
         </div>
       </div>
+      <div className={clsx('absolute left-[50%] top-0 rounded-lg px-3 py-1 text-sm font-medium pt-[4px] w-[80px] h-[40px] leading-[32px] bg-gray-700 align-middle transition-opacity duration-300', {'opacity-0': !toastVisible, 'opacity-1': toastVisible })}>复制成功</div>
     </>
   );
 }
